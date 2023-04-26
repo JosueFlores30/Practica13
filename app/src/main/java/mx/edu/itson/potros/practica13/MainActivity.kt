4d6804cd15922b0bcf0373f6ab5597795b35fb49
JosueFlores30 <josue.flores228704@potros.itson.edu.mx>
2023-04-25 21:11:34 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/itson/potros/practica13/MainActivity.kt
@@ -0,0 +1,22 @@
+package mx.edu.itson.potros.practica13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import mx.edu.itson.potros.practica13.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+    }
+}
